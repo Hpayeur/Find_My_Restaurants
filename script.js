64c3ef05b8da10934adf5d3993fb6eb8c7fa8e7b
Hpayeur <235752572+Hpayeur@users.noreply.github.com>
2025-12-16 13:01:26 -0700

    changing everything to fit

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
-       let cards = document.querySelector("div.cards");
-       let para = document.createElement("p");
-       if (!cards) {
-           cards = document.createElement("div");
-           cards.className = "cards";
-           document.body.appendChild(cards);
-       }
-        
-        
-        const URL = 'https://iskarr.github.io/austindonovan.github.io/api/business.json';
-        console.log('Fetching data from:', URL);
+// const URL = 'https://iskarr.github.io/austindonovan.github.io/api/business.json';
+// let cards = document.querySelector("div.cards");
+// let para = document.createElement("p");
+
+// fetch(URL)
+//         .then((response) => response.json())
+//         .then((jsObject) => {
+//           let business = jsObject.business;
+//                 for (let i = 0; i < business.length; i++) {
+//                         console.log(business[i].name);
+// let card = document.createElement("section");
+// card.className = "card";
+// let h3 = document.createElement("h3");
+// h3.textContent = business[i].name;
+// card.appendChild(h3);
+// document.querySelector("div.cards").appendChild(card);
+//           }
+//           });
+
+
+
+const URL =
+"https://iskarr.github.io/austindonovan.github.io/api/business.json";
+let cards = document.querySelector("div.cards");
+let para = document.createElement("p");
 fetch(URL)
 .then((response) => response.json())
 .then((jsObject) => {
-console.log(jsObject);
-let cards = document.createElement("div");
-cards.className = "cards";
+let business = jsObject.business;
+for (let i = 0; i < business.length; i++) {
+console.log(business[i].name);
+cards = document.createElement("food-cards");
+// Adds a classname to the section element above
+cards.className = "card";
 let h3 = document.createElement("h3");
-h3.textContent = (jsObject && jsObject.business && jsObject.business.length > 0) ? jsObject.business[0].name : "";
+h3.textContent = business[i].name;
 cards.appendChild(h3);
 document.querySelector("div.cards").appendChild(cards);
+}
 });
+
+// // Create the image location, you can look up how to add image using javascript
+
+// // Include a business location
+
+// // Include a business Description
