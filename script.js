e84a6082b2bb83e8db727f83393fb5e737b986ab
Hpayeur <235752572+Hpayeur@users.noreply.github.com>
2025-12-16 13:25:03 -0700

    images

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
-// const URL = 'https://iskarr.github.io/austindonovan.github.io/api/business.json';
-// let cards = document.querySelector("div.cards");
-// let para = document.createElement("p");
-
-// fetch(URL)
-//         .then((response) => response.json())
-//         .then((jsObject) => {
-//           let business = jsObject.business;
-//                 for (let i = 0; i < business.length; i++) {
-//                         console.log(business[i].name);
-// let card = document.createElement("section");
-// card.className = "card";
-// let h3 = document.createElement("h3");
-// h3.textContent = business[i].name;
-// card.appendChild(h3);
-// document.querySelector("div.cards").appendChild(card);
-//           }
-//           });
-
-
-
-const URL =
-"https://iskarr.github.io/austindonovan.github.io/api/business.json";
+const URL ="https://iskarr.github.io/austindonovan.github.io/api/business.json";
 let cards = document.querySelector("div.cards");
 let para = document.createElement("p");
 fetch(URL)
 .then((response) => response.json())
 .then((jsObject) => {
-let business = jsObject.business;
-for (let i = 0; i < business.length; i++) {
-console.log(business[i].name);
-cards = document.createElement("food-cards");
-// Adds a classname to the section element above
-cards.className = "card";
-let h3 = document.createElement("h3");
-h3.textContent = business[i].name;
-cards.appendChild(h3);
-document.querySelector("div.cards").appendChild(cards);
-}
-});
+let business = jsObject.business;});
+
+// const URL =
+// "https://iskarr.github.io/austindonovan.github.io/api/business.json";
+
+// let cards = document.querySelector("div.cards");
+// let para = document.createElement("p");
+
+// fetch(URL)
+// .then((response) => response.json())
+// .then((jsObject) => {
+// let business = jsObject.business;
+
+// for (let i = 0; i < business.length; i++) {
+// // console.log(business[i].name);
+// // You must include your javascript below to display the following information:
+// // 1. business names, 2.business image,
+// // 3. business locations, 4. business descriptions
+// // Here is an example for the name to start you off.
+
+// // Creates card and places business name in h2 element
+// let card = document.createElement("section");
+// // Adds a classname to the section element above
+// card.className = "card";
+// let h2 = document.createElement("h2");
+// h2.textContent = business[i].name;
+// card.appendChild(h2);
+// document.querySelector("div.cards").appendChild(card);
 
 // // Create the image location, you can look up how to add image using javascript
 
 // // Include a business location
 
 // // Include a business Description
+// }
+// });
