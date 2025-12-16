ed68d7c01ceca47d9913b69dd7a286999f2aa140
Hpayeur <235752572+Hpayeur@users.noreply.github.com>
2025-12-16 14:26:41 -0700

    fixed?

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
-const URL ="https://iskarr.github.io/austindonovan.github.io/api/business.json";
+
+const URL =
+"https://iskarr.github.io/austindonovan.github.io/api/business.json";
 let cards = document.querySelector("div.cards");
 let para = document.createElement("p");
 fetch(URL)
 .then((response) => response.json())
 .then((jsObject) => {
-let business = jsObject.business;});
+let business = jsObject.business;
+for (let i = 0; i < business.length; i++) {
+console.log(business[i].name);
+        cards = document.createElement("div");
+        cards.className = "business-card";
+let h3 = document.createElement("h3");
+h3.textContent = business[i].name;
+        cards.appendChild(h3);
+document.querySelector('h3').appendChild(cards);
+}
+});
 
-// const URL =
-// "https://iskarr.github.io/austindonovan.github.io/api/business.json";
-
-// let cards = document.querySelector("div.cards");
-// let para = document.createElement("p");
-
-// fetch(URL)
-// .then((response) => response.json())
-// .then((jsObject) => {
-// let business = jsObject.business;
-
-// for (let i = 0; i < business.length; i++) {
-// // console.log(business[i].name);
-// // You must include your javascript below to display the following information:
-// // 1. business names, 2.business image,
-// // 3. business locations, 4. business descriptions
-// // Here is an example for the name to start you off.
-
-// // Creates card and places business name in h2 element
-// let card = document.createElement("section");
-// // Adds a classname to the section element above
-// card.className = "card";
-// let h2 = document.createElement("h2");
-// h2.textContent = business[i].name;
-// card.appendChild(h2);
-// document.querySelector("div.cards").appendChild(card);
+async function fetchBusinessData(){
+        const response = await fetch(URL);
+        const data = await response.json();
+        console.log(data);
+        if (data.business && data.business.length > 0) {
+            document.querySelector('#business-name').textContent = data.business[0].name;
+            document.querySelector('#business-location').textContent = data.business[0].location;
+            document.querySelector('#business-description').textContent = data.business[0].description;
+        }
+        return;
+}
 
 // // Create the image location, you can look up how to add image using javascript
 
 // // Include a business location
 
 // // Include a business Description
-// }
-// });
